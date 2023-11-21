085ed8d0f44fafbf1a8f82d1e6151303f3540144
Dewy310 <150258693+Dewy310@users.noreply.github.com>
2023-11-21 14:27:43 +0200

    Add files via upload

---
+++ 3.2/3.2/3.2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double formula_z1(double alpha) {
+    // Формула z1 = cos(alpha) + sin(alpha) + cos(3*alpha) + sin(3*alpha)
+    double z1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha);
+    return z1;
+}
+
+double formula_z2(double alpha) {
+    // Формула z2 = 2√2 * cos(alpha) * sin(π/4 + 2*alpha)
+    double z2 = 2 * sqrt(2) * cos(alpha) * sin(3.14159265358979323846 / 4 + 2 * alpha);
+    return z2;
+}
+
+int main() {
+    // Приклад використання:
+    double alpha_value;
+
+    cout << "Введіть значення для кута alpha (в радіанах): ";
+    cin >> alpha_value;
+
+    // Обчислення та виведення результатів
+    double result_z1 = formula_z1(alpha_value);
+    double result_z2 = formula_z2(alpha_value);
+
+    cout << "За формулою z1 отримано: " << result_z1 << endl;
+    cout << "За формулою z2 отримано: " << result_z2 << endl;
+
+    return 0;
+}
